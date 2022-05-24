8231ba5d3c6de1f26b88797ad204faab325d1132
21a3084 <106131138+21a3084@users.noreply.github.com>
2022-05-24 15:11:28 +0900

    Update app3.py

--- app3.py
+++ app3.py
@@ -17,7 +17,8 @@ for i in range(50):
   latest_iteration.text(f'Iteration{i+1}'+'   ^_^   まもなく...')
   bar.progress(i+1)
   time.sleep(0.1)
-
+  if(i==50):
+    latest_iteration.text(f'Iteration{i+1}')
 'Done!!!!!!!'
 left_column,right_column =st.columns(2)
 button=left_column.button('右カラムに文字を表示')
